ef9775995409fd4ffa843cd001659f6f24be02cf
Michael Bisgaard Olesen <mo@concordium.com>
2022-09-14 16:25:46 +0200

    Work

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignClient from "@walletconnect/sign-client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const walletConnectProjectId = "76324905a70fe5c388bab46d3e0564dc";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+SignClient.init({
+    projectId: walletConnectProjectId,
+}).then(client => {
+    window.walletConnect = client;
+
+    const root = ReactDOM.createRoot(
+        document.getElementById('root') as HTMLElement
+    );
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
+});
