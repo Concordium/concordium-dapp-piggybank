2a40338415f3cd8ccc4b83e6a5070bc125a11e5d
Michael Bisgaard Olesen <mo@concordium.com>
2023-06-04 13:39:42 +0200

    Bump dapp-libraries dependency

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,6 @@
-import { WithWalletConnector } from '@concordium/react-components';
+import { TESTNET, WithWalletConnector } from '@concordium/react-components';
 import React from 'react';
 import App from './App';
-import { TESTNET } from './config';
 
 export default function Root() {
     return <WithWalletConnector network={TESTNET}>{(props) => <App {...props} />}</WithWalletConnector>;
